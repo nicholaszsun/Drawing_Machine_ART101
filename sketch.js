32f5cf4af74b2545b618c36bdc247f18aae069f0
nicholasz-sun <nicholas.sun@sjsu.edu>
2022-11-08 12:10:16 -0800

    updated sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let x = 15
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(220);
+}
+
+function keyTyped() {
+  if (key == ']') {
+    x = x + 15; //if I press ] my brush will go up by 15
+  } else {
+    (key == '[');
+    x = x - 15; //if I press [ my brush will go down by 15
+    // } else if (x < 0) {
+    //   console.log('too small')
+    // }
+  }
 }
 
 function draw() {
-  background(220);
+  strokeWeight(x); //adjust the strokeweight by pressing right bracket
+  if (mouseIsPressed) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
